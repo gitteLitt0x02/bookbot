bb685caa95d1149c77055a59c33e6f21fde5540d
gitteLitt0x02 <olavadamsrod@gmail.com>
2024-10-03 16:50:50 +0200

    updated main.py with more functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 def get_book_text(book_path):
     with open(book_path) as file:
         return file.read()
@@ -15,23 +17,41 @@ def word_counter(file_content, delimiters = ' \t\n'):
     if word:
         result.append(word)
     #print("myOwnSplit: ", result)
-    return (len(result))
+    count = len(result)
 
-def counter_split(file_content):
-    split = file_content.split()
-    print("splitCount: ", len(split))
+    return result, count
+
+
 
 def char_counter(file_content):
-    pass
-    
+    lowered = file_content.lower()
+    letters = {}
+    for char in lowered:
+        if char not in letters:
+            letters[char] = 1
+        else:
+            letters[char] += 1
+    return letters
+
+def char_presenter(char_func):
+    for k,v in char_func.items():
+        
+        if m := re.match("([a-z])", k):
+            print(f"The '{m.group(1)}' character was found {v} times")
+
+
 def main():
     book_path = "books/frankenstein.txt"
     file_content = get_book_text(book_path)
-    print(word_counter(file_content))
-    #sprint(file_content)
-    word_counter(file_content)
-    counter_split(file_content)
+    word_func = word_counter(file_content)
+    char_func = char_counter(file_content)
+    #print(word_func[0], '\n')
+    #print(word_func[1])
 
+    print(f'--- Begin report of {book_path} ---\n')
+    print(f'{word_func[1]} words found in the document \n')
+    char_presenter(char_func)
+    print('--- End report ---')
 
 if __name__ == "__main__":
     main()
